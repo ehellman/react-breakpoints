bc9bed49bcd7361f01c0891865f94e952d57fa5a
Erik Hellman <iam@erikhellman.com>
2017-08-19 18:59:44 +0200

    Add error messages for when too short arrays are passed as props

---
+++ modules/messages.js
@@ -0,0 +1,4 @@
+export const ERRORS = {
+  NO_CONNECTED_BREAKPOINTS: 'ConnectedBreakpoints requires you to pass an array of 3 nume,ric breakpoint values or more, see documentation at: https://github.com/ehellman/react-breakpoints',
+  NO_BREAKPOINTS: 'Breakpoints requires you to pass an array of 3 nume,ric breakpoint values or more, see documentation at: https://github.com/ehellman/react-breakpoints'
+}
