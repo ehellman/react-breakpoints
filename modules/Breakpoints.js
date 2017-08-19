ec2e3c3b0b8167370b736d7316e0f0e260d012f2
Erik Hellman <iam@erikhellman.com>
2017-08-19 19:00:12 +0200

    Modify the Breakpoints component to support custom arrays of 3 numerical values or more

--- modules/Breakpoints.js
+++ modules/Breakpoints.js
@@ -1,61 +1,57 @@
 import React from 'react'
+import { ERRORS } from './messages'
 
 class Breakpoints extends React.Component {
   constructor() {
     super()
     this.state = {
       currentBreakpoint: null,
-      width: null,
       breakpoints: []
     }
     this.calculateBreakpoint = this.calculateBreakpoint.bind(this)
-    this.setBreakpoints = this.setBreakpoints.bind(this)
   }
   componentWillMount() {
-    window.addEventListener('resize', this.updateWidth.bind(this))
-    window.addEventListener('load', this.updateWidth.bind(this))
-    !this.props.breakpoints && this.setState({ 
-      breakpoints: [
-        320,
-        480,
-        768,
-        992,
-        1200,
-        1560 
-      ]
-    })
+    if (!this.props.breakpoints || this.props.breakpoints.length <= 2) throw new Error(ERRORS.NO_BREAKPOINTS)
+    this.setState({ breakpoints: this.props.breakpoints })
+    window.addEventListener('resize', this.readWidth.bind(this))
+    window.addEventListener('load', this.readWidth.bind(this))
   }
   componentWillUnmount() {
-    window.removeEventListener('resize', this.updateWidth.bind(this))
-    window.removeEventListener('load', this.updateWidth.bind(this))
+    window.removeEventListener('resize', this.readWidth.bind(this))
+    window.removeEventListener('load', this.readWidth.bind(this))
   }
-  setBreakpoints(breakpoints) {
-    return this.props.breakpoints && this.setState({ breakpoints })
-  }
-  updateWidth(event) {
+  readWidth(event) {
     let width = event.target.innerWidth
       ? event.target.innerWidth
       : window.innerWidth
-    this.setState({ width })
     this.calculateBreakpoint(width)
   }
   calculateBreakpoint(width) {
-    if (width < this.state.breakpoints[1]) {
-      this.setState({ currentBreakpoint: 1 })
-    } else if (width >= this.state.breakpoints[0] && width < this.state.breakpoints[2]) {
-      this.setState({ currentBreakpoint: 2 })
-    } else if (width >= this.state.breakpoints[1] && width < this.state.breakpoints[3]) {
-      this.setState({ currentBreakpoint: 3 })
-    } else if (width >= this.state.breakpoints[2] && width < this.state.breakpoints[4]) {
-      this.setState({ currentBreakpoint: 4 })
-    } else if (width >= this.state.breakpoints[3] && width < this.state.breakpoints[5]) {
-      this.setState({ currentBreakpoint: 5 })
-    } else if (width >= this.state.breakpoints[4] && width > this.state.breakpoints[5]) {
-      this.setState({ currentBreakpoint: 6 })
+    if (this.state.breakpoints.length > 2) {
+      
+      this.state.breakpoints.map((breakpoint, i) => {
+
+        if (i == 0 && width < this.state.breakpoints[i + 1]) {
+          this.state.currentBreakpoint != i && this.setState({ currentBreakpoint: i })
+        }
+        if (i >= 1 && i < this.state.breakpoints.length) {
+          if (width >= this.state.breakpoints[i - 1] && width < this.state.breakpoints[i]) {
+            this.state.currentBreakpoint != i - 1 && this.setState({ currentBreakpoint: i - 1 })
+          }
+        }
+        if (i == this.state.breakpoints.length - 1) {
+          if (width >= this.state.breakpoints[i]) {
+            this.state.currentBreakpoint != i && this.setState({ currentBreakpoint: i })
+          }
+        }
+        
+      })
+
     }
+
   }
   render() {
-    return this.props.children
+    return React.cloneElement(this.props.children, { currentBreakpoint: this.state.currentBreakpoint })
   }
 }
 
